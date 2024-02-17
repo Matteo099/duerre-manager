568eb5e777b9ef92a25809fb65722c05b2b525d2
Matteo Romagnoli <matteo02.romagnoli@studenti.unicam.it>
2024-02-17 09:55:19 +0100

    feat: Implemented erase tool

--- src/main/webui/src/app/die-editor/manager/tools/eraser-tool-handler.ts
+++ src/main/webui/src/app/die-editor/manager/tools/eraser-tool-handler.ts
@@ -3,61 +3,93 @@ import { IDieEditor } from "../idie-editor";
 import { ToolHandler } from "./tool-handler";
 import { ERASABLE } from "../constants";
 import Konva from "konva";
+import { IFrame } from "konva/lib/types";
 
 const FADE_DURATION = 0.5; // in seconds
 const FRAME_RATE = 60; // Frames per second
 
 export class EraserToolHandler extends ToolHandler {
 
-    private readonly trail: Konva.Shape;
+    private readonly trail: Konva.Line;
+    private readonly anim: Konva.Animation;
     private readonly shapesToDelete: (Konva.Shape | Konva.Stage | Konva.Node)[] = [];
-    private pos: Konva.Vector2d = { x: 50, y: 50 };
+    private mouseDown = false;
 
     constructor(editor: IDieEditor) {
         super(editor);
 
-        this.trail = new Konva.Shape({
-            stroke: 'black',
+        const objs = this.createObjects();
+        this.trail = objs.trail;
+        this.anim = objs.anim;
+    }
+
+    private createObjects(): { trail: Konva.Line, anim: Konva.Animation } {
+        const trail = new Konva.Line({
+            stroke: 'rgba(0.15,0.15,0.15,0.2)',
             strokeWidth: 5,
             globalCompositeOperation: 'source-over',
-            // round cap for smoother lines
             lineCap: 'round',
             lineJoin: 'round',
-            sceneFunc: (ctx, shape) => {
-                console.log("sceneFunc")
-                ctx.beginPath();
-                ctx.moveTo(this.pos.x, this.pos.y);
-                ctx.arc(this.pos.x, this.pos.y, 3, 0, Math.PI * 2, true);
-                ctx.fillStrokeShape(shape);
-            }
+            points: [],
         });
+        this.editor.layer.add(trail);
+
+        let acc = 0;
+        const anim = new Konva.Animation((frame?: IFrame) => {
+            if (!frame) return;
+
+            acc += frame.timeDiff;
+            if (acc > 15) {
+                const points = this.trail.points();
+                const size = points.length > 8 ? Math.round(points.length / 4) : 2;
+                points.splice(0, size % 2 == 0 ? size : size + 1);
+                this.trail.points(points);
+                acc = 0;
+            }
+        }, this.editor.layer);
+
+        return { trail, anim };
     }
 
     override onToolSelected(): void {
         super.onToolSelected();
+        this.trail.moveToTop();
+        this.anim.start();
         //this.editor.layer.toggleHitCanvas();
     }
 
     override onToolDeselected(): void {
         super.onToolDeselected();
+        this.anim.stop();
         //this.editor.layer.toggleHitCanvas();
     }
 
     override onMouseDown(event: KonvaEventObject<any>): void {
-        this.editor.layer.add(this.trail);
+        const pos = this.editor.stage.getRelativePointerPosition();
+        if (!pos) return;
+
+        this.mouseDown = true;
+        this.trail.points([pos.x, pos.y, pos.x, pos.y]);
+
         this.addToDeletedShapes(this.getEraseableNode(event));
     }
 
     override onMouseMove(event: KonvaEventObject<any>): void {
+        if (!this.mouseDown) return;
+
+        // prevent scrolling on touch devices
+        event.evt.preventDefault();
         const pos = this.editor.stage.getRelativePointerPosition();
-        console.log(pos);
         if (!pos) return;
 
-        this.pos = pos;
+        const newPoints = this.trail.points().concat([pos.x, pos.y]);
+        this.trail.points(newPoints);
         this.addToDeletedShapes(this.getEraseableNode(event));
     }
 
     override onMouseUp(event: KonvaEventObject<any>): void {
+        this.mouseDown = false;
+
         this.shapesToDelete.forEach(s => {
             this.editor.state.remove(s);
         });
@@ -65,18 +97,18 @@ export class EraserToolHandler extends ToolHandler {
 
     private getEraseableNode(event: KonvaEventObject<any>): Konva.Shape | Konva.Stage | Konva.Node | undefined {
         const erasableParents = event.target?.findAncestors((n: Konva.Node) => n.getAttr(ERASABLE));
-        return erasableParents && erasableParents.length > 0 ? erasableParents[erasableParents.length - 1] : event.target;
+        return erasableParents && erasableParents.length > 0 ? erasableParents[erasableParents.length - 1] : (event.target.getAttr(ERASABLE) ? event.target : undefined);
     }
 
     private addToDeletedShapes(node?: Konva.Shape | Konva.Stage | Konva.Node) {
-        let s = true;
-        if (!node || s) return;
+        if (!node || this.shapesToDelete.includes(node)) return;
 
         this.shapesToDelete.push(node);
         this.makeGray(node);
     }
 
-    private makeGray(node: Konva.Shape | Konva.Stage | Konva.Node) {
-
+    private makeGray(node: Konva.Shape | Konva.Stage | Konva.Node | Konva.Group) {
+        node.cache();
+        node.filters([Konva.Filters.Grayscale]);
     }
 }
